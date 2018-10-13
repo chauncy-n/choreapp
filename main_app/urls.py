4f7ccbfebffa989be4f294b4083719b049724df4
chauncy-n <eldoon@comcast.net>
2018-10-13 15:25:55 -0700

    added templates forchores indexand main app index

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('chores/', views.chores_index, name='choress_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,17 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'base.html')
+
+class Chore:
+    def __init__(self, task, description):
+        self.task = task
+        self.description = description
+
+chores = [
+    Chore('clean room', 'put everything away'),
+    Chore('brush teeth', 'brush all your teeth not just two'),
+]
+
+
+def chores_index(request):
+    return render(request, 'chores/index.html', {'chores': chores})
