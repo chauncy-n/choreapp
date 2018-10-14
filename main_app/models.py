200540b2e3469e7ec3b69631f55866ecbc7da3ec
chauncy-n <eldoon@comcast.net>
2018-10-13 17:52:48 -0700

    adding models

--- main_app/models.py
+++ main_app/models.py
@@ -2,3 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Chore(models.Model):
+    task = models.CharField(max_length=50)
+    description = models.CharField()
+    points = models.IntegerField()
