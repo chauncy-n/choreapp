e0faa7c8072936f8adf9756fac904e0889a3d9a5
chauncy-n <eldoon@comcast.net>
2018-10-14 10:29:56 -0700

    added a chore module and tested it, created a superuser

--- main_app/models.py
+++ main_app/models.py
@@ -5,5 +5,8 @@ from django.contrib.auth.models import User
 
 class Chore(models.Model):
     task = models.CharField(max_length=50)
-    description = models.CharField()
+    description = models.CharField(max_length=1000)
     points = models.IntegerField()
+
+    def __str__(self):
+        return self.task
--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Chore
 
 # Create your views here.
 
 def index(request):
     return render(request,'base.html')
 
-class Chore:
-    def __init__(self, task, description):
-        self.task = task
-        self.description = description
-
-chores = [
-    Chore('clean room', 'put everything away'),
-    Chore('brush teeth', 'brush all your teeth not just two'),
-]
-
-
 def chores_index(request):
+    chores = Chore.objects.all()
     return render(request, 'chores/index.html', {'chores': chores})
