6e879ed3cb119eedd08fbb613253c3c758db959a
chauncy-n <eldoon@comcast.net>
2018-10-15 11:59:56 -0700

    login and sign up does not work trying to fix

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('chores/', views.chores_index, name='chores_index'),
     path('chores/<int:chore_id>', views.chores_detail, name='chores_detail'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name="logout"),
+    path('signup/', views.signup, name='signup'),
+    path('user/<username>/', views.profile, name='profile'),
+    path('chores/create/', views.ChoreCreate.as_view(), name='chores_create'),
+    path('chores/<int:pk>/update/', views.ChoreUpdate.as_view(), name='chores_update'),
+    path('chores/<int:pk>/delete/', views.ChoreDelete.as_view(), name='chores_delete'),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Chore
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 # Create your views here.
+
+class ChoreCreate(CreateView):
+    model = Chore
+    fields = ['task', 'description', 'points']
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect('/chores/')
+
+class ChoreUpdate(UpdateView):
+    model = Chore
+    fields = ['task', 'description', 'points']
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object = self.request.user
+        self.object.save()
+        return HttpResponseRedirect('/chores/' + str(self.object.pk))
+
+@method_decorator(login_required, name='dispatch')
+class ChoreDelete(DeleteView):
+    model = Chore
+    success_url = '/chores'
 
 def index(request):
     return render(request,'base.html')
@@ -15,12 +48,51 @@ def chores_detail(request, chore_id):
     chore = Chore.objects.get(id=chore_id)
     return render(request, 'chores/detail.html', {'chore': chore})
 
-# class ChoreCreate(CreatView):
-#     model = Chore
-#     fields = [task, description, points]
+def login_view(request):
+    if request.method == 'POST':
+        # if post, then authenticate (user submitted username and password)
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+                else:
+                    print("The account has been disabled.")
+                    return HttpResponseRedirect('/')
+            else:
+                print("The username and/or password is incorrect.")
+                return HttpResponseRedirect('/')
+    else:
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
 
-#     def form_valid(self, form):
-#         self.object = form.save(commit=False)
-#         self.object = self.request.user
-#         self.object.save()
-#         return HttpResponseRedirect('/chores/')
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
+
+@login_required
+def profile(request, username):
+    if username == request.user.username:
+        user = User.objects.get(username=username)
+        chores = Chore.objects.filter(user=user)
+        return render(request, 'profile.html', {'username': username, 'chores': chores})
+    else:
+        return HttpResponseRedirect('/')
+
+
+    
